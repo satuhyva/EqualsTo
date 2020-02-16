ab6dc0d09a9693703f78b1c9b13cd53568c24eed
Satu Hyvärinen <hyvarinen.satu.i@gmail.com>
2020-02-16 12:17:06 +0200

    Updates

---
+++ precache-manifest.b1a73181e211691ba1aab61f7cc9c70d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9b9e7340c15f61c7f9446623f40dc7",
+    "url": "/EqualsTo/index.html"
+  },
+  {
+    "revision": "cdc13cc64bcec68213f7",
+    "url": "/EqualsTo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a355086af28027db4e86",
+    "url": "/EqualsTo/static/js/2.7d8fec66.chunk.js"
+  },
+  {
+    "revision": "bb0d9eec61ee5484410f03345faddf5d",
+    "url": "/EqualsTo/static/js/2.7d8fec66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc13cc64bcec68213f7",
+    "url": "/EqualsTo/static/js/main.12d569b9.chunk.js"
+  },
+  {
+    "revision": "3390b7457c8825eb1387",
+    "url": "/EqualsTo/static/js/runtime-main.477f1fc0.js"
+  }
+]);
